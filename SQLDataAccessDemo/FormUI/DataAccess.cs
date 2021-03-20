308f06797139a1a4591f555f180e1667835a1217
baileyccl51 <80834908+baileyccl51@users.noreply.github.com>
2021-03-20 12:47:19 +0000

    Add files via upload

---
+++ SQLDataAccessDemo/FormUI/DataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace FormUI
+{
+    public class DataAccess
+    {
+        public List<Student> GetStudents(string lastName)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("GPA")))
+            {
+                // var output = connection.Query<Student>($"select * from Student where LastName = '{ lastName }'").ToList();
+                var output = connection.Query<Student>("dbo.Student_GetByLastName @LastName", new { LastName = lastName }).ToList();
+                return output;
+            }             
+        }
+    }
+}
